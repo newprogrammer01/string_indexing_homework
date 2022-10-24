51e7c5c1ad7af0f1a2640618791d6aafa2205e54
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-24 14:41:01 +0500

    besh05

--- Index05.py
+++ Index05.py
@@ -1,9 +1,19 @@
+def isDigit(x):
+    if x>='0' and x<='9':
+        return True
+    else:
+        return False
 def main(s):
-    """
-    Given a variable s string of length five. Determine the number of digits involved in this variable.
-    Args:
-        s(str): parameter
-    Returns:
-        int: answer
-    """
-    return
+    k=0
+    if isDigit(s[0]):
+        k+=1
+    if isDigit(s[1]):
+        k+=1
+    if isDigit(s[2]):
+        k+=1
+    if isDigit(s[3]):
+        k+=1
+    if isDigit(s[4]):
+        k+=1
+    return k
+print(main("1xxx "))
